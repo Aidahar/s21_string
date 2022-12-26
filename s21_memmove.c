ad327efc6c1e50eb1f9c6baeed33e138d49722ab
trudieis <trudieis@student.21-school.ru>
2022-12-26 18:15:01 +0300

    change logic memmove

--- s21_memmove.c
+++ s21_memmove.c
@@ -7,7 +7,7 @@
 void *s21_memmove(void *dest, const void *src, size_t n) {
   size_t idx;
   if (NULL == dest) dest = NULL;
-  char *targ = malloc(n * sizeof(char));
+  char *targ = (char *)malloc(n * sizeof(char));
   const char *from = src;
   for (idx = 0; idx < n; idx++, targ++, from++) {
     *targ = *from;
